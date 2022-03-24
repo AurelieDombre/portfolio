00e586458de850d670ac75dffe0f052b536a5869
AurelieDombre <au.dombre@gmail.com>
2022-03-24 22:31:56 +0100

    page contact à terminer

---
+++ php/valider.php
@@ -0,0 +1,30 @@
+<?php
+include "...php";
+
+
+
+?>
+
+
+<main class="main-container_form">
+    <section>
+        <h2 class="page__title">Votre demande de contacte a été envoyée</h2>
+
+        <p class="form_message"> Mr/Mme <?= $_POST['firstname'] ?? '' ?> <?= $_POST['lastname'] ?? '' ?>,
+            <br> nous vous recontacterons dans les meilleurs délais à l'adresse : <br>
+            <?= $_POST['email'] ?? '' ?>
+
+
+            <br> Merci de votre confiance
+
+        <div id="signature" class="header__subtitle">
+            Votre cinéma
+        </div>
+        <div id="signature" class="header__maintitle">
+            Le Rodia
+        </div>
+
+        </div>
+
+    </section>
+</main>
