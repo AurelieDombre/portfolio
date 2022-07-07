757cb4d873b84cdedac412209e539f64eaa8be75
AurelieDombre <au.dombre@gmail.com>
2022-07-07 18:24:36 +0200

    page projet début du css

--- app/views/main/projects.tpl.php
+++ app/views/main/projects.tpl.php
@@ -1,12 +1,9 @@
 <section class="projets">
         <h3>PROJETS</h3>
-        <div class="slider">
-            <!-- <img class="projets__img_AI slide__image slider__img"
-                src="../portfolio/Pictures/matrix.jpg" alt="Image de création WEB" href="#">
-            <img class="projets__img_languages slide__image slider__img"
-                src="../portfolio/Pictures/code.jpg"
-                alt="Image languages Web" href="#"> -->
-            <button class="btn slider__btn" type="button" aria-label="Précédent">&lt;</button>
-            <button class="btn slider__btn" type="button" aria-label="Suivant">&gt;</button>
+        <div class="project_list">
+
+            <img class="" src="../assets/pictures/matrix.jpg" alt="Image de création WEB" href="#">
+            <img class="" src="../assets/pictures/code.jpg" alt="Image languages Web" href="#"> 
+
         </div>
     </section>
