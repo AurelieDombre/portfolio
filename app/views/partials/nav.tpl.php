8c4a8da74267a759bcad8be7a21bc6baa215b66b
AurelieDombre <au.dombre@gmail.com>
2022-08-24 18:16:13 +0200

    changement de la nav bar

--- app/views/partials/nav.tpl.php
+++ app/views/partials/nav.tpl.php
@@ -1,5 +1,22 @@
     <!-- Menu -->
+
     <nav>
-        <a class="nav_accueil" href="<?= $router->generate('main_home') ?>" title="Accueil général">Accueil</a>
-        <a class="nav_contact" href="<?= $router->generate('main_contactForm') ?>"> <img class="btn_contact" src="assets/pictures/Mail-Icon-White-on-Black.svg" alt="Mail" > </a>
+        <a href="<?= $router->generate('main_home') ?>" title="Accueil général">
+            <!-- this anchor text for link your home to another page -->
+            <div class="icon">
+                <img src="assets/pictures/home.png" alt="home" >
+            </div>
+            <div class="name"><span data-text="Home">Accueil</span></div>
+            <!-- we are create first menu item name home -->
+        </a>
+
+        <a href="<?= $router->generate('main_contactForm') ?>">
+            <!-- this anchor text for link your home to another page -->
+            <div class="icon">
+                <img src="assets/pictures/mail.png" alt="home" >
+            </div>
+            <div class="name"><span data-text="Contact">Contact</span></div>
+            <!-- we create first menu item name home -->
+        </a>
     </nav>
+</html>
