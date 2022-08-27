9488c32d3881e35c3214776d01578f480f928fd9
AurelieDombre <au.dombre@gmail.com>
2022-08-27 12:54:11 +0200

    projet card

--- app/views/main/home.tpl.php
+++ app/views/main/home.tpl.php
@@ -15,20 +15,20 @@ include __DIR__.'/../partials/nav.tpl.php';
         <h3>BIENVENUE</h3>
 
         <p> J'ai le plaisir de vous présenter dans ce portfolio les
-            projets que j'ai menez à bien. </p>
+            projets que j'ai réalisé. </p>
     </section>
 
     <section class="projets">
         <h3>PROJETS</h3>
         <div class="projets__card">
-            <img class="projets__card--img" src="assets/pictures/qualiextra.jpg" alt="Image de création WEB" href="#">
+            <img class="projets__card--img" src="assets/pictures/qualiextra-home.png" alt="Image de création WEB" href="#">
             <span> Qualiextra, expériences épicuriennes</span>
         </div>
 
         <div class="projets__card">
-        <img class="projets__card--img" src="assets/pictures/image_cv.jpg" alt="Image languages Web" href="#"> 
-        <span> Mon curriculum vitae </span>    
-    </div>    
+            <img class="projets__card--img" src="assets/pictures/img_cv.png" alt="Image languages Web" href="#"> 
+            <span> Mon curriculum vitae </span>    
+        </div>    
     </section>
 
     <a class="btn_contact" href="<?= $router->generate('main_contactForm') ?>">  </a>
